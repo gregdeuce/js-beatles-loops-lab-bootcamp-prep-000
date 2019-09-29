5bcaa4a757b15c0402054f0a5c1adb13021de32e
ultimate-proc-6487 <gregdeuce@gmail.com>
2019-09-29 14:29:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+function theBeatlesPlay(mus, inst) {
+ 
+
+
+var myArray = [];
+
+for (var i = 0; i < mus.length; i++) {
+  myArray.push(`${mus[i]} plays ${inst[i]}`);
+} return myArray;
+}
+
+function johnLennonFacts(array) {
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"];
+} while 
